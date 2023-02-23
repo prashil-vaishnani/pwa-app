e03170fcf09ffc3a8f6cb017e53134617f3a78e8
prashil_v <prashil.v@simformsolutions.com>
2023-02-23 11:44:54 +0530

    set navbar with bootstrap and route setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import Counter from './component/counter';
+import React from "react";
+//import Counter from './component/counter';
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Users from "./pages/Users";
 
 function App() {
   return (
     <div>
-    <p>cra app</p>
-     <Counter/>
+      <Router>
+        <Navbar bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand>PWA</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/users">
+                Users
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="users/*" element={<Users />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
