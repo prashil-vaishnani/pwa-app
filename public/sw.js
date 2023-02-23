3bec028cf2328c9f4065bcd4700c6b9d90420ec3
prashil_v <prashil.v@simformsolutions.com>
2023-02-23 16:23:43 +0530

    handle offline mode behaviour with service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,27 @@
-console.log("it's working!! tension not ");
+//console.log("it's working!! tension not ");
+let cacheData = "v1";
+
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        "/",
+        "index.html",
+        "/static/js/bundle.js",
+        "favicon.ico",
+        "manifest.json",
+        "logo512.png",
+        "logo192.png",
+      ]);
+    })
+  );
+});
+this.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      if (res) {
+        return res;
+      }
+    })
+  );
+});
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 //import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import swDec from "./swDev";
 
@@ -16,4 +15,3 @@ root.render(
 
 //serviceWorkerRegistration.register();
 swDec();
-reportWebVitals();
